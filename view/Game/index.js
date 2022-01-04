f4abfe77ea84e841ed20739f63d45761a1bb9585
duerotkd <dureotkd123@naver.com>
2022-01-04 21:57:40 +0900

    Firebase FCM 공부중

--- view/Game/index.js
+++ view/Game/index.js
@@ -1,95 +1,162 @@
-import { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  useWindowDimensions,
-  ScrollView,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, Button, Platform, AppState } from "react-native";
 import { connect } from "react-redux";
-import { SafeAreaView, SafeScrollView } from "react-native-safe-area-context";
-import { Button, ButtonGroup, withTheme } from "react-native-elements";
-import { Image, Input } from "react-native-elements";
 
-function Game({ navigation, loginUser, route }) {
-  const seq = route?.params?.seq || 0;
-  const layout = useWindowDimensions();
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
-  const [reply, setReply] = useState("");
-  const [replies, setReplies] = useState([]);
+/*
+API DOCS 
+PUSH : https://docs.expo.dev/versions/latest/sdk/notifications/ 
+APP-STATE : https://docs.expo.dev/versions/latest/react-native/appstate/
+*/
+function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
-  const handleRegReply = () => {
-    if (reply === "") {
-      alert("입력하세요..");
-      return;
-    }
-    const cloneReplies = [...replies];
-    cloneReplies.push(reply);
-    setReplies(cloneReplies);
+  useEffect(() => {
+    AppState.addEventListener("change", _handleAppStateChange);
+
+    // PUSH Devices Token
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    // 알림 이벤트
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    // 푸쉬 알림 클릭시
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("사용자가 푸쉬 클릭을 하였습니다.");
+      });
+
+    return () => {
+      AppState.removeEventListener("change", _handleAppStateChange);
+
+      // Background 환경에서 이벤트 삭제
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  const _handleBackground = () => {
+    console.log("aa?");
+
+    console.log(new Date());
   };
 
-  const handleReply = (value) => {
-    console.log(value);
+  const _handleAppStateChange = (nextAppState) => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      console.log("App has come to the foreground!");
+    }
 
-    setReply(value);
+    appState.current = nextAppState;
+
+    setAppStateVisible(appState.current);
+
+    if (appState.current === "background") {
+      _handleBackground();
+    }
+
+    console.log("AppState", appState.current);
   };
 
   return (
-    <View>
-      <ScrollView style={{ padding: 10 }}>
-        <Text Text>안녕하세요 {seq}번호 이미지 입니다</Text>
-        <Image
-          style={{ width: "100%", height: layout.height / 1.8 }}
-          source={{ uri: `https://source.unsplash.com/random?sig=${seq}` }}
-        />
-        <View>
-          {replies &&
-            replies.map((reply, key) => {
-              return (
-                <View key={key}>
-                  <Text>{reply}</Text>
-                </View>
-              );
-            })}
-        </View>
-        <View
-          style={{
-            marginTop: 100,
-            display: "flex",
-            flex: 1,
-            flexDirection: "column",
-          }}
-        >
-          <Input
-            onChangeText={handleReply.bind(this)}
-            placeholder="BASIC INPUT"
-          />
-          <TouchableOpacity
-            onPress={handleRegReply}
-            style={{
-              padding: 12,
-              backgroundColor: "yellow",
-              borderRadius: 6,
-            }}
-          >
-            <Text>전송</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
     </View>
   );
 }
 
-function ChangeState(state) {
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 1 },
+  });
+}
+
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
+
+const changeToState = (state) => {
   return {
     loginUser: state.loginUser,
   };
-}
+};
 
-const styles = StyleSheet.create({
-  container: {},
-});
-
-export default connect(ChangeState)(Game);
+export default connect(changeToState)(App);
