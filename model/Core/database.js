1e8d7cd72a4489b4eb6b386cebe3bcd5f5304648
성민 <63378895+dureotkd@users.noreply.github.com>
2022-01-07 18:08:19 +0900

    Update database.js

--- model/Core/database.js
+++ model/Core/database.js
@@ -4,7 +4,7 @@ const db = mysql.createPoolCluster();
 db.add("soundGame", {
   host: "localhost",
   user: "root",
-  password: "@slsksh33@",
+  password: "",
   database: "soundGame",
   port: 3306,
 });
